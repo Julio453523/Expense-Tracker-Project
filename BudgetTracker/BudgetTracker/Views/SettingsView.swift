ab6fad5f387f24fea9b41c06d30f9cea226a1187
Julio453523 <89619045+Julio453523@users.noreply.github.com>
2023-12-12 01:45:10 -0800

    Update SettingsView.swift

--- BudgetTracker/BudgetTracker/Views/SettingsView.swift
+++ BudgetTracker/BudgetTracker/Views/SettingsView.swift
@@ -20,53 +20,6 @@ struct SettingsView: View {
           TextField("Last Name", text: $last)
           DatePicker("Birthdate:", selection: $birthday)
         }
-        Section(header: Text("Social Media")) {
-            Button(action: {
-              if let url = URL(string: "https://www.instagram.com/ricky.truckner/") {
-                UIApplication.shared.open(url)
-              } else {
-                print("Invalid URL format")
-              }
-            }) {
-              HStack { 
-                Image(systemName: "camera")
-                Text("Instagram")
-                  .font(.body)
-                Spacer()
-              }
-            }
-            Button(action: {
-                if let url = URL(string: "https://github.com/Ricky-Truckner/Expense-Tracker") {
-                  UIApplication.shared.open(url)
-                } else {
-                  print("Invalid URL format")
-                }
-              }) {
-                HStack { // Wrap Text in HStack
-                  Image(systemName: "server.rack")
-                  Text("Github")
-                    .font(.body)
-                  Spacer()
-                }
-              }
-            Button(action: {
-            if let url = URL(string: "https://www.facebook.com/ricky.truckner/") {
-              UIApplication.shared.open(url)
-            } else {
-              print("Invalid URL format")
-            }
-          }) {
-            HStack {
-              Image(systemName: "m.circle")
-              Text("Meta")
-                .font(.body)
-              Spacer()
-            }
-          }
-           
-          
-          .frame(width: 200, height: 30)
-        }
         Section(header: Text("Display")) {
           Toggle(isOn: $darkModeManager.isDarkMode, label: {
             Text("Dark Mode")
